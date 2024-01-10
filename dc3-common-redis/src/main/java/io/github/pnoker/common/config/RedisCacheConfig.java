e71e3085406d3b40209392f60dd99ba4f2caf571
pnoker <pnokers@icloud.com>
2024-01-10 17:12:14 +0800

    feat: .

---
+++ dc3-common-redis/src/main/java/io/github/pnoker/common/config/RedisCacheConfig.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2016-present the IoT DC3 original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.pnoker.common.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.annotation.Resource;
+import java.time.Duration;
+
+/**
+ * Redis Cache
+ *
+ * @author pnoker
+ * @since 2022.1.0
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.cache.redis")
+public class RedisCacheConfig extends CachingConfigurerSupport {
+
+    @Resource
+    private RedisConnectionFactory factory;
+
+    @Setter
+    private Duration timeToLive;
+
+    /**
+     * 自定义缓存 Key 生成策略
+     *
+     * @return KeyGenerator
+     */
+    @Bean
+    public KeyGenerator firstKeyGenerator() {
+        return (target, method, params) -> params[0].toString();
+    }
+
+    /**
+     * 自定义缓存 Key 生成策略
+     *
+     * @return KeyGenerator
+     */
+    @Bean
+    public KeyGenerator commonKeyGenerator() {
+        final String dot = ".";
+        final String hashTag = "#";
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(dot);
+            sb.append(method.getName());
+            sb.append(hashTag);
+            for (Object obj : params) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+        };
+    }
+
+    /**
+     * 自定义 RedisCacheManager 类，主要是设置序列化，解决乱码问题
+     *
+     * @return CacheManager
+     */
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.builder(factory);
+
+        // Json 序列化配置
+        JsonMapper jsonMapper = JsonMapper.builder()
+                .findAndAddModules()
+                .activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, Boolean.FALSE)
+                .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, Boolean.TRUE)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, Boolean.FALSE)
+                .configure(MapperFeature.DEFAULT_VIEW_INCLUSION, Boolean.FALSE)
+                .serializationInclusion(JsonInclude.Include.NON_NULL).build();
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        serializer.setObjectMapper(jsonMapper);
+
+        // 配置 Key & Value 序列化
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+                .disableCachingNullValues().entryTtl(timeToLive);
+        return builder.cacheDefaults(config).build();
+    }
+
+}
